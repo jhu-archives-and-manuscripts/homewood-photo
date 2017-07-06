e6abdbf3e6335407f25e8796e178884c989b1ab9
Lora Davis <lorajdavis@gmail.com>
2017-07-06 15:36:22 -0400

    postAgents

---
+++ aspace-ingest/postAgents.py
@@ -0,0 +1,60 @@
+import json, requests, csv, time, secrets
+
+startTime = time.time()
+
+# import secrets
+baseURL = secrets.baseURL
+user = secrets.user
+password = secrets.password
+
+# test for successful connection
+def test_connection():
+	try:
+		requests.get(baseURL)
+		print 'Connected!'
+		return True
+
+	except requests.exceptions.ConnectionError:
+		print 'Connection error. Please confirm ArchivesSpace is running.  Trying again in 10 seconds.'
+
+is_connected = test_connection()
+
+while not is_connected:
+	time.sleep(10)
+	is_connected = test_connection()
+
+#authenticate
+auth = requests.post(baseURL + '/users/'+user+'/login?password='+password).json()
+session = auth["session"]
+headers = {'X-ArchivesSpace-Session':session, 'Content_Type':'application/json'}
+
+# File name of csv
+agents_csv = 'ASagents_EE.csv'
+
+# Open csv, create new csv
+csv_dict = csv.DictReader(open(agents_csv))
+f=csv.writer(open('new_ASagents_EE.csv', 'wb'))
+f.writerow(['primary_name']+['subordinate_name_1']+['uri'])
+
+# Construct JSON to post from csv
+agentList = []
+for row in csv_dict:
+    primary_name = row['primary_name']
+    subordinate_name_1 = row['subordinate_name_1']
+    agentRecord = {'publish': True}
+    agentRecord['names'] = [{'primary_name': primary_name, 'sort_name': primary_name + '. ' + subordinate_name_1, 'subordinate_name_1': subordinate_name_1, 'source': 'local', 'rules': 'rda'}]
+    agentRecord = json.dumps(agentRecord)
+    post = requests.post(baseURL + '/agents/corporate_entities', headers=headers, data=agentRecord).json()
+    print post
+    # Save uri to new csv file
+    uri = post['uri']
+    f.writerow([primary_name]+[subordinate_name_1]+[uri])
+
+# Feedback to user
+print 'New .csv saved to working directory.'
+
+# show script runtime
+elapsedTime = time.time() - startTime
+m, s = divmod(elapsedTime, 60)
+h, m = divmod(m, 60)
+print 'Post complete.  Total script run time: ', '%d:%02d:%02d' % (h, m, s)
